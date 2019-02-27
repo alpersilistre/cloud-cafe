a4aa78856c5d2e39ea29edde66be10268b2b6be6
Alper Silistre <alpersilistre@gmail.com>
2019-02-28 01:21:04 +0300

    Add cafe functionality added

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const cafeList = document.querySelector('#cafe-list');
+const form = document.querySelector('#add-cafe-form');
 
 function renderCafe(doc) {
     let li = document.createElement('li');
@@ -15,8 +16,21 @@ function renderCafe(doc) {
     cafeList.appendChild(li);
 }
 
+
+// getting data
 db.collection('cafes').get().then((snapshot) => {
     snapshot.docs.forEach(doc => {
         renderCafe(doc);
     });
+});
+
+// saving data
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    db.collection('cafes').add({
+        name: form.name.value,
+        city: form.city.value
+    });
+    form.name.value = '';
+    form.city.value = '';
 });
